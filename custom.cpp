7392fd5b4cbe73892d226d7bc733341840002e39
Xiong Jun <adairjun@gmail.com>
2015-09-30 14:56:52 +0800

    MOD: little modify

--- custom.cpp
+++ custom.cpp
@@ -13,32 +13,32 @@ using namespace boost::asio;
 
 int main()
 {
-	io_service ios;
-	ip::address addr = ip::address::from_string("127.0.0.1");
-	ip::tcp::endpoint ep(addr, 8888);
-	
-	ip::tcp::socket sock(ios);
-	sock.connect(ep);
-
-	iRequest f;
-	f.set_name("iRequest_name");
-	f.set_family("iRequest_family");
-	f.set_country("iRequest_country");	
-	string request;
-	f.SerializeToString(&request);
-	// 发送请求给服务端
-	sock.write_some(buffer(request));
-
-	// 接收服务端的请求
-	char response[100];
-	sock.read_some(buffer(response));
-	
-	string serstr(response);		
-	iResponse s;
-	s.ParseFromString(response);
-	cout << s.classmate().c_str() << endl;
-	cout << s.school().c_str() << endl;
-	cout << s.teacher().c_str() << endl;
-	
-	return 0;
+  io_service ios;
+  ip::address addr = ip::address::from_string("127.0.0.1");
+  ip::tcp::endpoint ep(addr, 8888);
+  
+  ip::tcp::socket sock(ios);
+  sock.connect(ep);
+  
+  iRequest f;
+  f.set_name("iRequest_name");
+  f.set_family("iRequest_family");
+  f.set_country("iRequest_country");	
+  string request;
+  f.SerializeToString(&request);
+  // 发送请求给服务端
+  sock.write_some(buffer(request));
+  
+  // 接收服务端的请求
+  char response[100];
+  sock.read_some(buffer(response));
+  
+  string serstr(response);		
+  iResponse s;
+  s.ParseFromString(response);
+  cout << s.classmate().c_str() << endl;
+  cout << s.school().c_str() << endl;
+  cout << s.teacher().c_str() << endl;
+  
+  return 0;
 }
--- service.cpp
+++ service.cpp
@@ -13,34 +13,32 @@ using namespace boost::asio;
 
 int main()
 {
-	io_service ios;
-	ip::address addr = ip::address::from_string("127.0.0.1");
-	ip::tcp::endpoint ep(addr, 8888);
-	ip::tcp::acceptor acce(ios, ep);
-	
-
-	iResponse s;
-	s.set_classmate("iResponse_classmate");
-	s.set_school("iResponse_school");
-	s.set_teacher("iResponse_teacher");
-	string response;
-	s.SerializeToString(&response);
-
-	while(true)
-	{
-		ip::tcp::socket sock(ios);
-		acce.accept(sock);
-		
-		char request[100];
-		sock.read_some(buffer(request));
-		iRequest f;
-		f.ParseFromString(string(request));	
-		cout << f.name().c_str() << endl;
-		cout << f.family().c_str() << endl;
-		cout << f.country().c_str() << endl;
-		sock.write_some(buffer(response));
-	}
-
-	return 0;
+  io_service ios;
+  ip::address addr = ip::address::from_string("127.0.0.1");
+  ip::tcp::endpoint ep(addr, 8888);
+  ip::tcp::acceptor acce(ios, ep);
+  
+  iResponse s;
+  s.set_classmate("iResponse_classmate");
+  s.set_school("iResponse_school");
+  s.set_teacher("iResponse_teacher");
+  string response;
+  s.SerializeToString(&response);
+  
+  while (true) {
+  	ip::tcp::socket sock(ios);
+  	acce.accept(sock);
+  	
+  	char request[100];
+  	sock.read_some(buffer(request));
+  	iRequest f;
+  	f.ParseFromString(string(request));	
+  	cout << f.name().c_str() << endl;
+  	cout << f.family().c_str() << endl;
+  	cout << f.country().c_str() << endl;
+  	sock.write_some(buffer(response));
+  }
+  
+  return 0;
 }
 		
