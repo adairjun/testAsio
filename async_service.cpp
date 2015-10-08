e813611249a997ebf1599fc8ec70563f08fd5af8
Xiong Jun <adairjun@gmail.com>
2015-10-08 15:40:40 +0800

    MOD: modify async_service.cpp:

--- async_service.cpp
+++ async_service.cpp
@@ -11,55 +11,61 @@
 #include <boost/system/error_code.hpp>
 #include "start.pb.h"
 
-using namespace std;
 using namespace boost::asio;
-using namespace boost::system;
-using namespace boost;
+using boost::system::error_code;
+using ip::tcp;
 
-void write_handler(const system::error_code&)
+struct CHelloWorld_Service
 {
-  cout << "send msg complete" << endl;
-}
+        CHelloWorld_Service(io_service &iosev)
+                :m_iosev(iosev),m_acceptor(iosev, tcp::endpoint(ip::address::from_string("127.0.0.1"), 8888))
+        {}
 
-void accept_handler(const system::error_code& ec, string response, boost::shared_ptr<ip::tcp::socket> sock_pt)
-{
-  if (ec) { return; }
-  sock_pt->async_write_some(buffer(response), boost::bind(write_handler, _1));
-}
+        void start()
+        {
+                // 开始等待连接（非阻塞）
+                boost::shared_ptr<tcp::socket> psocket(new tcp::socket(m_iosev));
+                // 触发的事件只有error_code参数，所以用boost::bind把socket绑定进去
+                m_acceptor.async_accept(*psocket,
+                        boost::bind(&CHelloWorld_Service::accept_handler, this, psocket, _1));
+        }
 
-int main()
+        // 有客户端连接时accept_handler触发
+        void accept_handler(boost::shared_ptr<tcp::socket> psocket, error_code ec)
+        {
+                if(ec) return;
+                // 继续等待连接
+                start();
+                // 显示远程IP
+                std::cout << psocket->remote_endpoint().address() << std::endl;
+                // 发送信息(非阻塞)
+                boost::shared_ptr<std::string> pstr(new std::string("hello async world!"));
+                psocket->async_write_some(buffer(*pstr),
+                        boost::bind(&CHelloWorld_Service::write_handler, this, pstr, _1, _2));
+        }
+
+        // 异步写操作完成后write_handler触发
+        void write_handler(boost::shared_ptr<std::string> pstr, error_code ec,
+                size_t bytes_transferred)
+        {
+                if(ec)
+                std::cout<< "发送失败!" << std::endl;
+                else
+                std::cout<< *pstr << " 已发送" << std::endl;
+        }
+
+        private:
+                io_service &m_iosev;
+                ip::tcp::acceptor m_acceptor;
+};
+
+int main(int argc, char* argv[])
 {
-  io_service ios;
-  ip::address addr = ip::address::from_string("127.0.0.1");
-  ip::tcp::endpoint ep(addr, 8888);
-  ip::tcp::acceptor acce(ios, ep);
-  
-  while (true) {
-	boost::shared_ptr<ip::tcp::socket> sock_pt(new ip::tcp::socket(ios));
-  	//ip::tcp::socket sock(ios);
-  	//acce.accept(sock);
-	iResponse s;
-	s.set_classmate("iResponse_classmate");
-	s.set_school("iResponse_school");
-	s.set_teacher("iResponse_teacher");
-	string response;
-	s.SerializeToString(&response);
-  
-	acce.async_accept(*sock_pt, boost::bind(accept_handler, _1, response, sock_pt));
-	/*  	
-  	char request[100];
-  	sock.read_some(buffer(request));
-  	iRequest f;
-  	f.ParseFromString(string(request));	
-  	cout << f.name().c_str() << endl;
-  	cout << f.family().c_str() << endl;
-  	cout << f.country().c_str() << endl;
-  	sock.write_some(buffer(response));
-	*/ 
-    // 异步等待	
-	ios.run();
-  }
-  
-  return 0;
+        io_service iosev;
+        CHelloWorld_Service sev(iosev);
+        // 开始等待连接
+        sev.start();
+        iosev.run();
+
+        return 0;
 }
-		
